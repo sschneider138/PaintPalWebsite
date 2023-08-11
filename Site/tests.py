e8f15007023d869430b1984ebd942e2297cbfdb3
LoLynne <134532671+LoLynne@users.noreply.github.com>
2023-08-10 19:41:45 -0500

    Update tests.py
    I got this test to run on my fork  now trying now send to the branch

--- Site/tests.py
+++ Site/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+import unittest
+
+class PainCalculations:
+
+    def __init__(self, height, width, unit,profile=None, windowHeight=0, windowWidth=0):
+      self.height = height
+      self.width = width
+      self.unit = unit
+      self.profile = profile
+      self.windowHeight = windowHeight
+      self.windowWidth = windowWidth
+
+class TestPaintCalculations(unittest.TestCase):
+   def test_initialization(self):
+      paint_calc =PainCalculations(10, 5, 'm','profile1',2, 1)
+      self.assertEqual(paint_calc.width, 5) 
+      self.assertEqual(paint_calc.unit, 'm')
+      self.assertEqual(paint_calc.profile,'profile1')
+      self.assertEqual(paint_calc.windowHeight, 2)
+      self.assertEqual(paint_calc.windowWidth, 1) 
+
+if __name__ == '__main__': 
+   unittest.main()        
 
 # Create your tests here.
